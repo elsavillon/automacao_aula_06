171db4857fd290c36cc04ded7b416aa07e6749d3
Elsa Villon <144967593+elsavillon@users.noreply.github.com>
2024-03-21 12:24:18 -0300

    atualização

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
 import os
-import requests
 from flask import Flask, request
+import requests
 
-app =Flask(__name__)
-BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
 
-@app.route("/")
+app = Flask(__name__)
+BOT_TOKEN = os.environ['TELEGRAM_BOT_TOKEN']
+
+@app.route('/')
 def index():
-  return "Olá, <b> tudo bem?</b>"
+  return 'Olá, <b>tudo bem?</b>'
 
-@app.route("/teste")
-def teste():
-  return "Essa página é um <b>teste</b>"
+@app.route('/teste')
+def sobre():
+  return 'Sobre o <strong>projeto</strong>'
 
-@app.route("telegram", methods=["POST"])
+@app.route('/telegram', methods="POST")
 def telegram_update():
   update = request.json
-  url_envio_mensaagem = f"https://api.telegram.org/bot{BOT_TOKEN0}/sendMessage"
-  chat_id = update["message"]["chat"]["id"]
-  mensagem = {"chat_id: chat_id", "text":"mensagem <b>recebida</b>!", "parse mode": "HTML"}
-  requests.post(url_envio_mensagem, data=mensagem)
-  return "ok"
+  url_envio_mensagem = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage'
+  chat_id = update['message']['chat']['id']
+  mensagem = {'chat_id': chat_id, 'text': 'Mensagem <b>recebida</b>', 'parse_mode': 'html'}
+  request.post(url_envio_mensagem, data=mensagem)
+  return 'ok'
