42dd6184e38279fe626593528a82dab480e5b1da
Elsa Villon <144967593+elsavillon@users.noreply.github.com>
2024-03-20 22:17:15 -0300

    atualização

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask
+import os
+import requests
+from flask import Flask, request
 
 app =Flask(__name__)
+BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
 
 @app.route("/")
 def index():
@@ -9,3 +12,12 @@ def index():
 @app.route("/teste")
 def teste():
   return "Essa página é um <b>teste</b>"
+
+@app.route("telegram", methods=["POST"])
+def telegram_update():
+  update = request.json
+  url_envio_mensaagem = f"https://api.telegram.org/bot{BOT_TOKEN0}/sendMessage"
+  chat_id = update["message"]["chat"]["id"]
+  mensagem = {"chat_id": chat_id, "text": "mensagem <b>recebida</b>!, "parse mode": "HTML"}
+  requests.post(url_envio_mensagem, data=mensagem)
+  return "ok"
